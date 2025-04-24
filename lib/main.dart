60df514f87a7d76aefcef99dbf1bfc77e0704327
Charlie Rostant YOSSA <Charlieyossa@gmail.com>
2025-04-24 23:39:08 +0100

    Change App name

--- lib/main.dart
+++ lib/main.dart
@@ -94,7 +94,7 @@ class MyApp extends ConsumerWidget {
         );
       },
       debugShowCheckedModeBanner: false,
-      title: 'Openlib',
+      title: 'Megalib',
       theme: lightTheme,
       darkTheme: darkTheme,
       themeMode: themeMode,
@@ -138,7 +138,7 @@ class _MainScreenState extends ConsumerState<MainScreen> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.surface,
-        title: const Text("Openlib"),
+        title: const Text("Megalib"),
         titleTextStyle: Theme.of(context).textTheme.displayLarge,
       ),
       body: _widgetOptions.elementAt(selectedIndex),
